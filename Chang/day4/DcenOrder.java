de2c9362b46ee846c949ccca7673d7b09645dce4
de123456sdf <cdaeun95@gmail.com>
2023-11-02 01:36:14 +0900

    4day 창다은

--- Chang/day4/DcenOrder.java
+++ Chang/day4/DcenOrder.java
@@ -1,5 +1,6 @@
 package Chang.day4;
 
+//n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴하는 함수
 public class DcenOrder {
 
     public long solution (long n) {
--- Chang/day4/PowCompare.java
+++ Chang/day4/PowCompare.java
@@ -1,5 +1,6 @@
 package Chang.day4;
 
+//n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 작성
 public class PowCompare {
 
     public long solution(long n) {
