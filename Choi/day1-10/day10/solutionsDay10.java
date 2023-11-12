c1166550202d272b14781d6060e90572d6cc1a6f
Youkamii <gkfkd747@naver.com>
2023-11-12 22:30:48 +0900

    2023년 11월 12일 day10 최혁

--- Choi/day1-10/day10/solutionsDay10.java
+++ Choi/day1-10/day10/solutionsDay10.java
@@ -1,48 +1,93 @@
 //public class solutionsDay10 {
 //
-//	// 문자열 내림차순으로 배치하기
-//	public String solution(String s) {
-//		String answer = "";
-//		char[] tmp = s.toCharArray();
-//		Arrays.sort(tmp);
-//		for (int i = s.length() - 1; 0 <= i; i--)
-//			answer = answer + tmp[i];
-//		return answer;
-//	}
+//	//행렬의 덧셈
+//	//1113
+//	public int[][] solution34(int[][] arr1, int[][] arr2) {
+//		int[][] answer = arr1;
 //
-//	// 문자열 내림차순으로 배치하기 2
-//	public String solution(String s) {
-//		char[] tmp = s.toCharArray();
-//		Arrays.sort(tmp);
-//		return new StringBuilder(new String (tmp)).reverse().toString();
-//	}
-//
-//	//부족한 금액 계산하기
-//	public long solution(int price, int money, int count) {
-//
-//		long answer = money * -1;
-//		//(count * (count + 1) / 2
-//		for (int i = 0; i < count; i++)
-//			answer += (long)price * (i + 1);
-//
-//		return answer > 0 ? answer : 0;
-//	}
-//
-//	public boolean solution(String s) {
-//
-//		char[] tmpArray = s.toCharArray();
-//
-//		if (s.length() != 4 && s.length() != 6) {
-//			return false;
-//		}
-//
-//		for (int i : tmpArray) {
-//			if (!('0' <= i && i <= '9')) {
-//				return false;
+//		for (int i = 0; i < arr1.length; i++) {
+//			for (int j = 0; j < arr1[i].length; j++) {
+//				answer[i][j] = arr1[i][j] + arr2[i][j];
 //			}
 //		}
 //
-//		return true;
+//		return answer;
+//	}
+//
+//	// 직사각형 별 찍기
+//	//1113
+////	public static void main(String[] args) {
+////		Scanner sc = new Scanner(System.in);
+////		int a = sc.nextInt();
+////		int b = sc.nextInt();
+////
+////		for (int i = 0; i < b; i++) {
+////			for (int j = 0; j < a; j++)
+////				System.out.print('*');
+////			if (i != b-1)
+////				System.out.println();
+////		}
+////	}
+//
+//	// 최대 공약수와 최소 공배수
+//	//1113
+//	public int[] solution35(int n, int m) {
+//		int gcd = euclidean35(n, m);
+//		int lcm = n * m / gcd;
+//
+//		return new int[]{gcd, lcm};
+//	}
+//
+//	public int euclidean35 (int n, int m) {
+//		int j = 1;
+//
+//		while ( j > 0 ) {
+//			j = n % m;
+//			n = m;
+//			m = j;
+//		}
+//
+//		return n;
+//	}
+//
+//	//3진법 뒤집기 ////
+//	//1113
+//	public static int solution36(int n) {
+//		int answer = 0;
+//
+//		List<Integer> ternary = new ArrayList<>();
+//
+//		while (n > 0) {
+//			ternary.add(n % 3);
+//			n /= 3;
+//		}
+//
+//		int[] arrayTernary = ternary.stream().mapToInt(i -> i).toArray();
+//
+//		int base = 0;
+//		for (int i = arrayTernary.length  - 1; i >= 0; i--)
+//			answer += arrayTernary[i] * (int)Math.pow(3, base++);
+//
+//		return answer;
+//	}
+//
+//	//3진법 뒤집기
+//	//1113
+//	public static int solution36_1(int n) {
+//		int answer = 0;
+//
+//		List<Integer> ternary = new ArrayList<>();
+//
+//		while (n > 0) {
+//			ternary.add(n % 3);
+//			n /= 3;
+//		}
+//
+//		int base = 0;
+//		for (int i = ternary.size() - 1; i >= 0; i--)
+//			answer += ternary.get(i) * (int)(Math.pow(3, base++));
+//
+//		return answer;
 //	}
 //
 //}
